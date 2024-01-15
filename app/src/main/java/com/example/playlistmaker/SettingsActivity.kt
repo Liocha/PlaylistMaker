2d94d87cb878126c928726501d560087d0ab6ad4
Aleksey <mrLiocha@yandex.ru>
2024-01-15 08:00:37 +0300

    Работа над ошибками.

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -3,15 +3,14 @@ package com.example.playlistmaker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.LinearLayout
+import android.widget.TextView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val btnSettingsHeader = findViewById<LinearLayout>(R.id.settings_header)
+        val btnSettingsHeader = findViewById<TextView>(R.id.settings_header)
         btnSettingsHeader.setOnClickListener {
             val displayIntent = Intent(this, MainActivity::class.java)
             startActivity(displayIntent)
