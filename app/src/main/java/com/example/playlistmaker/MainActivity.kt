742125fecd1958bb0e738ba5857b4be005cd707f
Aleksey <mrLiocha@yandex.ru>
2024-01-28 04:09:18 +0300

    СПРИНТ 8. Тема 2. Context и Intent

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.playlistmaker
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
 
@@ -13,27 +11,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnSearch = findViewById<MaterialButton>(R.id.search_btn)
-//        val btnClickListener: View.OnClickListener = object : View.OnClickListener {
-//            override fun onClick(v: View?) {
-//                Toast.makeText(this@MainActivity, "Нажали на Поиск!", Toast.LENGTH_SHORT).show()
-//            }
-//        }
-//        btnSearch.setOnClickListener(btnClickListener)
         btnSearch.setOnClickListener {
             val displayIntent = Intent(this, SearchActivity::class.java)
             startActivity(displayIntent)
         }
 
-
         val btnMedia = findViewById<MaterialButton>(R.id.media_btn)
         btnMedia.setOnClickListener {
-            //Toast.makeText(this@MainActivity,"Нажали на Медиатека!", Toast.LENGTH_SHORT).show()
             val displayIntent = Intent(this, MediaActivity::class.java)
             startActivity(displayIntent)
         }
+
         val btnSettings = findViewById<MaterialButton>(R.id.settings_btn)
         btnSettings.setOnClickListener {
-            //Toast.makeText(this@MainActivity,"Нажали на Настройки!", Toast.LENGTH_SHORT).show()
             val displayIntent = Intent(this, SettingsActivity::class.java)
             startActivity(displayIntent)
         }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,9 +1,10 @@
 package com.example.playlistmaker
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +17,38 @@ class SettingsActivity : AppCompatActivity() {
             startActivity(displayIntent)
         }
 
+        val btnShareApp = findViewById<TextView>(R.id.btn_share_app)
+        btnShareApp.setOnClickListener {
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.default_text_in_messenger))
+                type = "text/plain"
+            }
+            val shareAppIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareAppIntent)
+        }
+
+        val btnWriteToSupport = findViewById<TextView>(R.id.btn_write_to_support)
+        btnWriteToSupport.setOnClickListener() {
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SENDTO
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.default_email)))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.default_email_subject))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.default_email_text))
+            }
+            val writeToSupportIntent = Intent.createChooser(sendIntent, null)
+            startActivity(writeToSupportIntent)
+        }
+
+        val btnTermsOfUse = findViewById<TextView>(R.id.btn_terms_of_use)
+        btnTermsOfUse.setOnClickListener {
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_VIEW
+                data = Uri.parse(getString(R.string.practicum_offer_link))
+            }
+            val termsOfUseIntent = Intent.createChooser(sendIntent, null)
+            startActivity(termsOfUseIntent)
+        }
     }
 }
