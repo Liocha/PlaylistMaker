37027a3c92f9321417b7386b7fa9446086e642df
Aleksey <mrLiocha@yandex.ru>
2024-02-12 14:40:41 +0300

    СПРИНТ 10

---
+++ app/src/main/java/com/example/playlistmaker/SearchItemAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.playlistmaker
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.example.playlistmaker.modal.Track
+
+class SearchItemAdapter(private val dataSet: Array<Track>) :
+    Adapter<SearchItemAdapter.SearchItemViewHolder>() {
+
+    class SearchItemViewHolder(rootView: View) : ViewHolder(rootView) {
+        private val artistName: TextView
+        private val trackName: TextView
+        private val trackTime: TextView
+        private val artworkUrl100: ImageView
+        private val radiusInPx = rootView.context.resources.getDimension(R.dimen.corner_radius_search_item_img).toInt()
+        init {
+            artistName = rootView.findViewById(R.id.track_name)
+            trackName = rootView.findViewById(R.id.artist_name)
+            trackTime = rootView.findViewById(R.id.track_time)
+            artworkUrl100 = rootView.findViewById(R.id.artwork_url_100)
+        }
+
+        fun bind(track: Track) {
+            artistName.text = track.artistName
+            trackName.text = track.trackName
+            trackTime.text = track.trackTime
+            Glide.with(artworkUrl100).load(track.artworkUrl100).centerCrop().transform(
+                RoundedCorners(radiusInPx)
+            )
+                .placeholder(R.drawable.placeholder_artwork).into(artworkUrl100)
+
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): SearchItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.search_item, parent, false)
+        return SearchItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: SearchItemViewHolder, position: Int) {
+        holder.bind(dataSet[position])
+    }
+
+    override fun getItemCount() = dataSet.size
+}
