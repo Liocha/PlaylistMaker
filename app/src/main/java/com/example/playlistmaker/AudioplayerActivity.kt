e8a743b2903fea9dfd9ad1cd82116b6085fcc30f
Aleksey <mrLiocha@yandex.ru>
2024-03-20 23:47:05 +0300

    СПРИНТ 13. Создаем экран «Аудиоплеер».

--- app/src/main/java/com/example/playlistmaker/AudioplayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/AudioplayerActivity.kt
@@ -1,6 +1,5 @@
 package com.example.playlistmaker
 
-import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.widget.ImageButton
@@ -9,8 +8,6 @@ import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.data.api.model.Track
@@ -34,7 +31,7 @@ class AudioplayerActivity : AppCompatActivity() {
         track?.let {
 
             val artistNameTextView = findViewById<TextView>(R.id.artistName)
-            val trackNameTextView =  findViewById<TextView>(R.id.trackName)
+            val trackNameTextView = findViewById<TextView>(R.id.trackName)
             val trackTimeTextView = findViewById<TextView>(R.id.trackTimeValue)
             val collectionNameTextView = findViewById<TextView>(R.id.collectionNameValue)
             val releaseDateTextView = findViewById<TextView>(R.id.releaseDateValue)
@@ -44,14 +41,15 @@ class AudioplayerActivity : AppCompatActivity() {
             val radiusInPx =
                 this.resources.getDimension(R.dimen.corner_radius_search_item_img).toInt()
 
-            fun getCoverArtwork() = it.artworkUrl100.replaceAfterLast('/',"512x512bb.jpg")
+            fun getCoverArtwork() = it.artworkUrl100.replaceAfterLast('/', "512x512bb.jpg")
 
             val formatter = DateTimeFormatter.ISO_DATE_TIME
             val releaseDate = LocalDateTime.parse(it.releaseDate, formatter)
 
             artistNameTextView.text = it.artistName
             trackNameTextView.text = it.trackName
-            trackTimeTextView.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(it.trackTimeMillis)
+            trackTimeTextView.text =
+                SimpleDateFormat("mm:ss", Locale.getDefault()).format(it.trackTimeMillis)
             collectionNameTextView.text = it.collectionName
             releaseDateTextView.text = releaseDate.year.toString()
             primaryGenreNameTextView.text = it.primaryGenreName
